8308cce6e046fa78868e50b4795f08a85897b198
GabrielBadra <gabrielfreitasalves9@gmail.com>
2023-08-21 15:46:35 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/modal/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package modal.dao;
+
+import java.util.List;
+
+import model.entiteis.Seller;
+
+public interface SellerDao {
+
+	void insert(Seller obj);
+	void uptade(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+}
